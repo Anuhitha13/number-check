c1506de9302fd0c92d9d06ffbea7a66761d40a55
Anuhitha13 <71086518+Anuhitha13@users.noreply.github.com>
2020-11-06 18:36:34 +0530

    Add files via upload

---
+++ number check.py
@@ -0,0 +1,9 @@
+num = int(input('enter a number:'))
+if num >= 0:
+   if num == 0:
+      print('zero')
+   else :
+        print('it is a positive number')
+else :
+     print('it is a negative number')        
+
